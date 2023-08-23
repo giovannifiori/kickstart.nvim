38a6c843464788d9d903ab82582260f32bdea5f0
Giovanni Fiori <giovannicfiori@gmail.com>
2023-08-23 11:38:13 -0400

    feat: use catppuccin theme

--- init.lua
+++ init.lua
@@ -95,10 +95,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
 
@@ -109,7 +109,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin-frappe',
         component_separators = '|',
         section_separators = '',
       },
