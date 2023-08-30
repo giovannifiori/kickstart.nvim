1f6530964fb38c773ee7966b675b00a6920bd5c2
Giovanni Fiori <giovannicfiori@gmail.com>
2023-08-29 22:28:18 -0400

    feat: enalbe ts autotag

--- init.lua
+++ init.lua
@@ -294,6 +294,10 @@ require('nvim-treesitter.configs').setup {
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
+  autotag = {
+    enable = true,
+  },
+
   highlight = { enable = true },
   indent = { enable = true },
   incremental_selection = {
