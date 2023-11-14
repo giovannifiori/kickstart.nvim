86389176b85234deb3971f99bc8a974936e1729b
Giovanni Fiori <giovannicfiori@gmail.com>
2023-11-14 14:17:56 -0400

    feat: use incsearch

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -3,7 +3,8 @@
 -- NOTE: You can change these options as you wish!
 
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
+vim.o.incsearch = true
 
 -- Make line numbers default
 vim.o.number = true
