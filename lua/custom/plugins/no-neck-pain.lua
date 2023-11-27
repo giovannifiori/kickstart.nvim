d5ad3cdd27ec6fd750ba1cca0c83b2d803142464
Giovanni Fiori <giovannicfiori@gmail.com>
2023-11-27 08:27:40 -0400

    try no-neck-pain

---
+++ lua/custom/plugins/no-neck-pain.lua
@@ -0,0 +1,14 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+	version = "*",
+	opts = {
+		width = 150,
+		buffers = {
+			wo = {
+				number = true,
+				relativenumber = true,
+				foldenable = true,
+			}
+		}
+	}
+}
