ec552073b9efa133b2fcaba0d81ae5b2aab9d600
Giovanni Fiori <giovannicfiori@gmail.com>
2023-12-28 16:28:47 -0400

    feat: format with null-ls on save

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,29 +1,52 @@
 return {
-	'nvimtools/none-ls.nvim',
-	config = function()
-		local null_ls = require("null-ls")
-		-- code action sources
-		local code_actions = null_ls.builtins.code_actions
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require 'null-ls'
 
-		-- diagnostic sources
-		local diagnostics = null_ls.builtins.diagnostics
+    -- autoformat
+    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
-		-- formatting sources
-		local formatting = null_ls.builtins.formatting
-		null_ls.setup({
-			sources = {
-				code_actions.eslint_d,
-				code_actions.gitsigns,
-				diagnostics.commitlint,
-				diagnostics.eslint_d,
-				diagnostics.golangci_lint,
-				formatting.black,
-				formatting.gofmt,
-				formatting.isort,
-				formatting.prettierd,
-				formatting.templ,
-				formatting.stylua,
-			}
-		})
-	end
+    -- code action sources
+    local code_actions = null_ls.builtins.code_actions
+    -- diagnostic sources
+    local diagnostics = null_ls.builtins.diagnostics
+    -- formatting sources
+    local formatting = null_ls.builtins.formatting
+
+    null_ls.setup {
+      on_attach = function(client, bufnr)
+        if client.supports_method 'textDocument/formatting' then
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format {
+                async = false,
+                bufnr = bufnr,
+                filter = function(cli)
+                  return cli.name == 'null-ls'
+                end,
+              }
+            end,
+          })
+        end
+      end,
+      sources = {
+        code_actions.eslint_d,
+        code_actions.gitsigns,
+        diagnostics.commitlint,
+        diagnostics.eslint_d,
+        diagnostics.golangci_lint,
+        formatting.black,
+        formatting.gofmt,
+        formatting.isort,
+        formatting.prettierd,
+        formatting.templ,
+        formatting.stylua,
+      },
+    }
+
+    vim.keymap.set('n', '<leader>ff', vim.lsp.buf.format, { desc = '[F]ormat [f]ile' })
+  end,
 }
