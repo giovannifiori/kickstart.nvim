1b611728c7c6f65bd86e236826ff50096c98c0dd
Giovanni Fiori <giovannicfiori@gmail.com>
2024-02-07 21:54:11 -0400

    fix incremental selection bindings

--- lua/treesitter-setup.lua
+++ lua/treesitter-setup.lua
@@ -39,10 +39,10 @@ vim.defer_fn(function()
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
       },
     },
     textobjects = {
