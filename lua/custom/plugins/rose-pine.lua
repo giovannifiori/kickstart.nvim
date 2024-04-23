f5a7d6de3373f2bdb44d6411df9c94e5fff1abbd
Giovanni Fiori <giovannicfiori@gmail.com>
2024-04-22 21:09:42 -0400

    use dark variant of rose-pine

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -3,13 +3,13 @@ return {
   name = 'rose-pine',
   config = function()
     require('rose-pine').setup {
-      dark_variant = 'main',
+      dark_variant = 'moon',
       styles = {
         transparency = true,
       },
     }
 
-    vim.o.background = 'light'
+    vim.o.background = 'dark'
 
     vim.cmd.colorscheme 'rose-pine'
   end,
