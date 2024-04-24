0b489cfed642dcf24e0ff4b328bda0bf4cc1ca83
Giovanni Fiori <giovannicfiori@gmail.com>
2024-04-23 22:11:46 -0400

    add persistence.nvim

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -24,6 +24,8 @@ return {
           { icon = '📦 ', desc = 'Package Manager', group = 'Label', action = 'Lazy', key = 'p' },
           { icon = '🔍 ', desc = 'Find Files', group = 'Label', action = 'Telescope find_files', key = 'f' },
           { icon = '📁 ', desc = 'File Browser', group = 'Label', action = 'Telescope file_browser', key = 'b' },
+          { icon = '🗃 ', desc = 'Restore Project Last Session', group = 'Label', action = 'lua require("persistence").load()', key = 'r' },
+          { icon = '🗃 🌎 ', desc = 'Restore Last Session', group = 'Label', action = 'lua require("persistence").load({ last = true })', key = 'R' },
         },
       },
     }
