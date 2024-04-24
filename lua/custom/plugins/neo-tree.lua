c8e2a660decd09498337515a6d3d93522f82ad94
Giovanni Fiori <giovannicfiori@gmail.com>
2024-04-24 17:04:06 -0400

    add neotree

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  config = function()
+    require('neo-tree').setup {
+      window = {
+        position = 'right',
+        mappings = {
+          ['O'] = {
+            command = function(state)
+              local node = state.tree:get_node()
+              local filepath = node.path
+              local osType = vim.loop.os_uname().sysname
+              local command
+              if osType == 'Darwin' then
+                command = 'open ' .. filepath
+              else
+                command = 'xdg-open ' .. filepath
+              end
+              os.execute(command)
+            end,
+            desc = "[O]pen file with system's default application",
+          },
+        },
+      },
+    }
+    vim.keymap.set('n', '<leader>ft', '<cmd>:Neotree reveal right toggle<CR>', { desc = 'Toggle [F]ile [T]ree' })
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+}
