f0e20e48f170189692d857baeacd77ce6fdf2615
Giovanni Fiori <giovannicfiori@gmail.com>
2024-05-16 12:19:34 -0400

    remove mini.comment in favor of 0.10 default

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
   version = false,
   config = function()
     require('mini.surround').setup()
-    require('mini.comment').setup()
     require('mini.cursorword').setup()
     require('mini.indentscope').setup()
     require('mini.pairs').setup()
